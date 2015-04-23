97c8abe6d50a18c337979ade2728b5b6e11f7e78
Roman Lapacherie <roman.lapacherie@gmail.com>
2015-04-23 17:04:26 +0100

    ajout des sprites et du déplacement du personnage

--- model.js
+++ model.js
@@ -1,25 +1,54 @@
 var Model = (function() {
-    "use strict";
+	"use strict";
 
-    var model = {
-	player : { x: null,
-		   y: null },
-	world : { width : 27,
-		  height: 12 }
-    };
-    
-    model.update = function() {
-	if (Input.left)  model.player.x -= 1;
-	if (Input.right) model.player.x += 1; 
-	if (Input.up)    model.player.y -= 1; 
-	if (Input.down)  model.player.y += 1; 
-		
-	if (model.player.x >= model.world.width ) model.player.x = 0;
-	if (model.player.y >= model.world.height) model.player.y = 0;
-	if (model.player.x < 0) model.player.x = model.world.width  - 1;
-	if (model.player.y < 0) model.player.y = model.world.height - 1;
-    };
+	var model = {
+		player : { 
+			x: null,
+			y: null,
+			direction: null,
+			moving: false 
+		},
+		world : { 
+			width : 27,
+			height: 12 
+		},
+		movable: true
+	};
 
-    return model;
-    
+	model.update = function() {
+		if (Input.left && model.movable)  {
+			model.player.x -= 1;
+			model.player.direction = "left";
+			model.player.moving = true;
+			
+		}
+		if (Input.right && model.movable) {
+			model.player.x += 1;
+			model.player.direction = "right";
+			model.player.moving = true;
+		} 
+		if (Input.up && model.movable)    {
+			model.player.y -= 1;
+			model.player.direction = "up";
+			model.player.moving = true;
+		} 
+		if (Input.down && model.movable)  {
+			model.player.y += 1; 
+			model.player.direction = "down";
+			model.player.moving = true;
+			
+		}
+
+		if (model.player.x >= model.world.width ) {
+			model.player.x = 0;
+		}
+		if (model.player.y >= model.world.height) {
+			model.player.y = 0;
+		}
+		if (model.player.x < 0) model.player.x = model.world.width  - 1;
+		if (model.player.y < 0) model.player.y = model.world.height - 1;
+	};
+
+	return model;
+
 }());
--- renderer.js
+++ renderer.js
@@ -1,60 +1,130 @@
 var Renderer = (function() {
-    "use strict";
+	"use strict";
 
-    var stage,
+	var stage,
 	background,
 	gridOverlay,
 	player;
 
-    var squareSize = 50;
-    
-    var buildGridOverlay = function() {
-	var grid = new createjs.Shape();
-	grid.graphics.beginStroke("white"); 
-	for (var i = 0 ; i < Model.world.width ; ++i) {
-	    grid.graphics
-		.moveTo(i*squareSize, 0)
-		.lineTo(i*squareSize, Model.world.height*squareSize-1);
-	}
-	for (var j = 0 ; j < Model.world.height ; ++j) {
-	    grid.graphics
-		.moveTo(0                 , j*squareSize)
-		.lineTo(Model.world.width*squareSize-1, j*squareSize);
-	}
-	grid.graphics.endStroke();
-	return grid;
-    };
+	var squareSize = 50;
+	var i= 0;
+	var time = 0;
 
-    var init = function() {
-	var canvas = document.getElementById("gameCanvas");
-	canvas.width  = Model.world.width  * squareSize;
-	canvas.height = Model.world.height * squareSize;
+	var buildGridOverlay = function() {
+		var grid = new createjs.Shape();
+		grid.graphics.beginStroke("white"); 
+		for (var i = 0 ; i < Model.world.width ; ++i) {
+			grid.graphics
+			.moveTo(i*squareSize, 0)
+			.lineTo(i*squareSize, Model.world.height*squareSize-1);
+		}
+		for (var j = 0 ; j < Model.world.height ; ++j) {
+			grid.graphics
+			.moveTo(0                             , j*squareSize)
+			.lineTo(Model.world.width*squareSize-1, j*squareSize);
+		}
+		grid.graphics.endStroke();
+		return grid;
+	};
 
-	stage = new createjs.Stage("gameCanvas");
-	
-	player = new createjs.Shape();
-	player.graphics.beginFill("black").drawRect(1, 1, squareSize-2, squareSize-2);
+	var init = function() {
+		var canvas = document.getElementById("gameCanvas");
+		canvas.width  = Model.world.width  * squareSize;
+		canvas.height = Model.world.height * squareSize;
 
-	background = new createjs.Shape();
-	background.graphics.beginFill("#9cf292").drawRect(0, 0,
-							  squareSize * Model.world.width,
-							  squareSize * Model.world.height);
-	
-	var grid = buildGridOverlay();
+		stage = new createjs.Stage("gameCanvas");
+
+
+
+		background = new createjs.Shape();
+		background.graphics.beginFill("#9cf292").drawRect(0, 0,
+			squareSize * Model.world.width,
+			squareSize * Model.world.height);
+		background.graphics.beginFill
+		var grid = buildGridOverlay();
+
+
+
+		var imgPerso1 = new Image();
+
+		imgPerso1.src = "img/characters/perso1.png";
+
+    // create spritesheet and assign the associated data.
+    var spriteSheet = new createjs.SpriteSheet({
+    	// image to use
+    	images: [imgPerso1], 
+    	// width, height & registration point of each sprite
+    	frames: {width: 50, height: 85, regX: 0, regY: 0}, 
+    	animations: {    
+    		up: 	[12, 15, "up", 0.250],
+    		down: 	[0, 3, "down", 0.250],
+    		right: 	[8,11, "right", 0.250],
+    		left: 	[4, 7, "left", 0.250],
+    	},
+    	framerate: 20
+
+
+    });
+
+    player = new createjs.Sprite(spriteSheet);
 	
 	stage.addChild(background, grid, player);
-    };
-    
-    var render = function() {
-	player.x = Model.player.x * squareSize;
-	player.y = Model.player.y * squareSize;
 	
-	stage.update();
-    };
+	};
 
-    return {
-	init  : init,
-	render: render
-    };
-    
+	var playerMove = function() {
+		var moveX = 0;
+		var moveY = 0;
+
+		switch(Model.player.direction){
+				case "up":
+					moveY = -1;
+					break;
+				case "down":
+					moveY = 1;
+					break;
+				case "left":
+					moveX = -1;
+					break;
+				case "right":
+					moveX = 1;
+					break;
+
+		}
+
+		if (i === 0 && Model.player.moving) {
+			Model.movable = false;
+			Model.player.moving = false;
+			player.gotoAndPlay(Model.player.direction);
+			++i;
+		}
+		else if(i === 4) {
+			i=0;
+			player.stop();
+			Model.movable = true;
+			player.x += moveX * squareSize/4;
+			player.y += moveY * squareSize/4;
+		}
+		else if (i > 0  && i < 4){
+			player.x += moveX * squareSize/4;
+			player.y += moveY * squareSize/4;
+			i++;
+		}
+	}
+
+	var render = function() {
+		
+		if(createjs.Ticker.getTime() - time > 2500/60){
+			time = createjs.Ticker.getTime();
+			playerMove();
+		}
+		
+		stage.update();
+	};
+	
+	return {
+		init  : init,
+		render: render
+	};
+
 })();
